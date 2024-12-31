0e9871ca989fa21954fdfb88a8f76edd360cc34f
madleenmueller <madleen.mueller@t-online.de>
2024-12-31 13:40:23 +0100

    added get function for quizzes

--- app/api/courses/[courseId]/quizzes/route.ts
+++ app/api/courses/[courseId]/quizzes/route.ts
@@ -1,4 +1,12 @@
-import { auth } from "@clerk/nextjs";
+// POST: Create a new quiz for a course
+// GET: Get ALL quizzes for a course 
+// Offene Frage: Sollten hier tatsächlich ALLE Quizzes für einen Kurs abgerufen werden?
+// Genügt es nach der course_id zu filtern oder brauchen wir weitere Filter?
+// Ist es egal, wie course_id benannt ist? Wenn nein, muss es
+// course_id oder courseId sein? Wäre es sinnvoll, die Benennung in der 
+// Datenbank zu ändern? 
+
+import { auth } from "@clerk/nextjs"; 
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
@@ -30,5 +38,31 @@ export async function POST(req: Request, { params }: { params: { course_id: stri
   } catch (error) { // Catch any errors
     console.log("[QUIZ]", error); 
     return new NextResponse("Internal Error", { status: 500 }); // return an error if something goes wrong
+  }
+}
+
+export async function GET(req: Request, { params }: { params: { course_id: string } }) {
+  try {
+    const { userId } = auth();
+    const { course_id } = params;
+
+    if (!userId || !isTeacher(userId)) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!course_id) {
+      return new NextResponse("Missing courseId", { status: 400 });
+    }
+
+    const quizzes = await db.quiz.findMany({ // Find all quizzes for a course
+      where: {
+        course_id: course_id, // Filter by course_id
+      },
+    });
+
+    return NextResponse.json(quizzes);
+  } catch (error) {
+    console.log("[QUIZZES_GET_ALL]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
