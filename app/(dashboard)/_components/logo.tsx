6fa7e5715b5f892af9bebd0ec54c2039cd41441c
AnjaWurli <anja.wurlitzer.coding@gmail.com>
2025-03-11 20:30:30 +0100

    logo issue

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import logo from "@/public/logo.png";
 
 export const Logo = () => {
-  return <Image height={40} width={40} alt="logo" src={logo} />;
+  return <Image height={40} width={40} alt="logo" src="/logo.png" />;
 };
